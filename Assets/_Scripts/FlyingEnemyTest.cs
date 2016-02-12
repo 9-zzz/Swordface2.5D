465aea5cbaa04b12c9267c9d265a8749bbb22c93
Shahan Akhter <shahan3.14@gmail.com>
2016-02-12 02:35:26 -0500

    started making the first flying enemy, did the basic for it, new scene.

---
+++ Assets/_Scripts/FlyingEnemyTest.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using System.Collections;
+
+public class FlyingEnemyTest : MonoBehaviour
+{
+    public int fHealth = 5;
+
+    public float startMoveSpeed;
+    public float startRotateSpeed;
+
+    public GameObject fBullet;
+
+    public bool activated = false;
+    public bool hasReachedZ = false;
+    public bool ready = false;
+
+    Vector3 startTargetPosition;
+    Transform sp;
+
+    // Use this for initialization
+    void Start()
+    {
+        sp = transform.GetChild(1).transform;
+
+        startTargetPosition = transform.position;
+        startTargetPosition.z = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!activated)
+        {
+
+            if (!hasReachedZ)
+            {
+                if (transform.position.z == 0)
+                    hasReachedZ = true;
+
+                transform.position = Vector3.MoveTowards(transform.position, startTargetPosition, Time.deltaTime * startMoveSpeed);
+            }
+
+            if (hasReachedZ) // and ready == false? // This just go 90 no further.
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, 0), startRotateSpeed * Time.deltaTime);
+
+                if (transform.rotation.y == 0)
+                {
+                    //hasReachedZ = false; // ??? To not all choroutine multiple times.
+                    ready = true;
+                }
+            }
+
+            if (ready)
+            {
+                StartCoroutine(WaitAndSpit(2.0f));
+                ready = false;
+                activated = true;
+            }
+        }
+    }
+
+    IEnumerator WaitAndSpit(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            Instantiate(fBullet, sp.position, sp.rotation);
+            //var flyBullet = Instantiate(fBullet, sp.position, sp.rotation) as GameObject;
+            //flyBullet.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(50, 20), ForceMode2D.Force);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Bullet2D")
+        {
+            if (fHealth <= 0)
+                Destroy(gameObject);
+
+            fHealth--;
+        }
+    }
+
+}
